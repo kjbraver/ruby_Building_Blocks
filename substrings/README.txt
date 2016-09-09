9e9a1a8ddd66099283f98714de5ebc52952786d5
Kyle Braver <kyle.braver@gmail.com>
2016-09-09 00:05:44 -0700

    Complete substrings program with basic testing.

---
+++ substrings/README.txt
@@ -0,0 +1,3 @@
+Given a single keyword or string containing multiple keywords and an array of valid substrings, this program will return a count of each match between each keyword and a substring.
+Arguments: substrings(string keyword, Array substrings)
+Return: A hash of all substring matches (Ex: substring1 => 2, substring2 => 5)
---
+++ substrings/substrings.rb
@@ -0,0 +1,24 @@
+def substring(keyword, substrings)
+	#check that keyword is a string && substrings is an array of strings
+	usage = "Usage: substring(String keyword, String[] substrings)" 
+	return usage if(!keyword.is_a? String or !substrings.is_a? Array)
+	substrings.each {|substring| return usage if !substring.is_a? String}
+
+	#prepare keyword array and return array
+	keywords = keyword.downcase.split
+	substring_matches = Hash.new(0)
+	
+	#iterate through keywords and substrings matching each substring against each key
+	keywords.each do |key|
+		substrings.each do |substring|
+			substring_matches[substring] += 1 if key.include?(substring)
+		end
+	end
+
+	return substring_matches
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substring("below", dictionary)
+p substring("Howdy partner, sit down! How's it going?", dictionary)
